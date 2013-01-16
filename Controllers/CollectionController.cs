1ba8c66d13efe8c7ec3816a20898a458dffeefa0
Fan Shi <ninputer@hotmail.com>
2013-01-16 13:44:42 +0800

    Add a test page to access SQL Azure

---
+++ Controllers/CollectionController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using elements.Models;
+
+namespace elements.Controllers
+{
+    public class CollectionController : Controller
+    {
+        private CollectionContext db = new CollectionContext();
+
+        //
+        // GET: /Collection/
+
+        public ActionResult Index()
+        {
+            return View(db.Collections.ToList());
+        }
+
+        //
+        // GET: /Collection/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Collection collection = db.Collections.Find(id);
+            if (collection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(collection);
+        }
+
+        //
+        // GET: /Collection/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Collection/Create
+
+        [HttpPost]
+        public ActionResult Create(Collection collection)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Collections.Add(collection);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(collection);
+        }
+
+        //
+        // GET: /Collection/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Collection collection = db.Collections.Find(id);
+            if (collection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(collection);
+        }
+
+        //
+        // POST: /Collection/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Collection collection)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(collection).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(collection);
+        }
+
+        //
+        // GET: /Collection/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Collection collection = db.Collections.Find(id);
+            if (collection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(collection);
+        }
+
+        //
+        // POST: /Collection/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Collection collection = db.Collections.Find(id);
+            db.Collections.Remove(collection);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/Collection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace elements.Models
+{
+    public enum CollectionType
+    {
+        PureForm,
+        PureFormMakable,
+        Compound,
+    }
+
+    public class Collection
+    {
+        [Key]
+        public int CollectionID { get; set; }
+
+        public int ElementNumber { get; set; }
+        public CollectionType Type { get; set; }
+        public string Description { get; set; }
+        public string Storage { get; set; }
+    }
+}
